1e56fbb4df23645e5aefc2dd024447ab1c83085f
varan06 <suba983@gmail.com>
2018-09-05 10:20:37 +0530

    Adding first test scripts

---
+++ GoogleTest.java
@@ -0,0 +1,12 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GoogleTest {
+public static void main(String[] args) {
+	System.setProperty("webdriver.gecko.driver", "C:\\Users\\hp\\Downloads\\geckodriver-v0.21.0-win64/geckodriver.exe");
+	WebDriver firefox= new FirefoxDriver();
+	firefox.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-server/3.13.0");
+}
+}
---
+++ Java.java
@@ -0,0 +1,6 @@
+package selenium;
+
+public class Java {
+	
+
+}
